3c8e35b8cc54e91a8960135a6117ab1f2d54872e
Staroyee <danmasefield@gmail.com>
2024-01-20 19:50:27 +1030

    Created basic components and pages

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import { Navbar, Container } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar className="navbar">
+        <Container>
+          <h1>Fabulous Tarot</h1>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <nav>Home</nav>
+  )
+}
+
+export default Home
