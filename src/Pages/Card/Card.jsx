97a41e260795228cd9b8acf5c5dcb14f9e859737
Staroyee <danmasefield@gmail.com>
2024-01-23 18:21:56 +1030

    Created getrandomcard function

--- src/Pages/Card/Card.jsx
+++ src/Pages/Card/Card.jsx
@@ -2,9 +2,23 @@ import { Container, Row, Col } from "react-bootstrap";
 import { motion } from "framer-motion";
 
 import CardBack from "../../assets/images/card-back.png";
+import CardBack2 from "../../assets/images/card-back-2.png";
+import CardBack3 from "../../assets/images/card-back-3.png";
 import "./card.css";
 
 const Card = () => {
+  const cardArray = [CardBack, CardBack2, CardBack3];
+
+  function getRandomCard() {
+    const randomIndex = Math.floor(Math.random() * cardArray.length);
+    return cardArray[randomIndex];
+  }
+
+  const handleCardClick = () => {
+    window.location.reload();
+  };
+
+  const randomCard = getRandomCard();
   return (
     <>
       <Container>
@@ -28,9 +42,10 @@ const Card = () => {
               <motion.div
                 transition={{ ease: "easeOut", duration: 0.4 }}
                 whileHover={{ scale: 1.02 }}
+                onClick={handleCardClick}
               >
                 <img
-                  src={CardBack}
+                  src={randomCard}
                   alt="front-of-tarot-card"
                   className="card-image"
                 />
