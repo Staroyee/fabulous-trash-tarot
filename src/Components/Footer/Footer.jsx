40375960c42cb6ac8e67a0b7cba706efec4b985b
Staroyee <danmasefield@gmail.com>
2024-01-24 16:08:14 +1030

    Created footer links

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,53 @@
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import "./footer.css";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <Container className="footer-background">
+        <Row>
+          <Col>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.fabuloustrash.com.au/"
+            >
+              Shop
+            </Link>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.fabuloustrash.com.au/about"
+            >
+              About
+            </Link>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.fabuloustrash.com.au/contact"
+            >
+              Contact Us
+            </Link>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.fabuloustrash.com.au/faq"
+            >
+              FAQ
+            </Link>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.fabuloustrash.com.au/wholesale"
+            >
+              Wholesale
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
